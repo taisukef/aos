41bd01bb2d6d584d3bd308431cc648b59bb4672e
Taisuke Fukuno <fukuno@jig.jp>
2021-07-05 06:55:05 +0900

    aos-js → aos-es

--- AOS.js
+++ AOS.js
@@ -4,18 +4,22 @@ import { css } from "https://js.sabae.cc/stdom.js";
 css("../aos.css");
 AOS.init();
 
-class AOSJS extends HTMLElement {
+class AOSES extends HTMLElement {
   constructor() {
     super();
     const options = {};
     const keys = Object.keys(AOS.options);
+    const pres = ["aos-", "data-aos-", ""];
     for (let i = 0; i < this.attributes.length; i++) {
       const a = this.attributes[i];
-      if (a.name.startsWith("aos-")) {
-        const an = a.name.substring(4);
-        const key = keys.find(k => k.toLowerCase() == an);
-        if (key) {
-          options[key] = a.value;
+      for (const pre of pres) {
+        if (a.name.startsWith(pre)) {
+          const an = a.name.substring(pre.length);
+          const key = keys.find(k => k.toLowerCase() == an);
+          if (key) {
+            options[key] = a.value;
+          }
+          break;
         }
       }
     }
@@ -23,6 +27,6 @@ class AOSJS extends HTMLElement {
   }
 }
 
-customElements.define("aos-js", AOSJS);
+customElements.define("aos-es", AOSES);
 
 export { AOS };
